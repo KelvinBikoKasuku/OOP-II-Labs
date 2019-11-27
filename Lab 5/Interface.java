fd6b9f36d569f8dbc5d894f845a0e790750554cb
KelvinBikoKasuku <46823486+KelvinBikoKasuku@users.noreply.github.com>
2019-11-27 16:29:35 +0300

    Add files via upload

---
+++ Lab 5/Interface.java
@@ -0,0 +1,15 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/**
+*<h1>Interface</h1> 
+ *This is the interface component of the Remote Invocation
+ */
+
+	/**
+		* This abstarct method references the UI to be run
+		* @return void This method returns nothing
+	*/
+public interface Interface extends Remote{
+	void animation() throws RemoteException;
+}
